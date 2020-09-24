281f5a7260f2d15a63e1d5ff2b761f279e983fa1
George Garces <george@Georges-MacBook-Pro.local>
2020-09-23 22:36:17 -0500

    Adds is valid builtin

--- src/builtins/regex.js
+++ src/builtins/regex.js
@@ -1,4 +1,16 @@
-re_match = (pattern, value) => RegExp(pattern).test(value);
+regexMatch = (pattern, value) => RegExp(pattern).test(value);
+regexIsValid = (pattern) => {
+    try {
+        "".match(new RegExp(pattern));
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
 regexSplit = (pattern, s) => s.split(RegExp(pattern));
 
-module.exports = { "regex.split": regexSplit, re_match };
+module.exports = {
+    "regex.match": regexMatch,
+    "regex.is_valid": regexIsValid,
+    "regex.split": regexSplit
+};
